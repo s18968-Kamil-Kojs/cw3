61fda7f74ddf97fa722d5edb743504754ee7648f
Kamil Kojs <kamil.kojs@gmail.com>
2020-03-30 16:53:48 +0200

    cw05 first commit

---
+++ APBD03/APBD03/Controllers/EnrollmentsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APBD03.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APBD03.Controllers {
+
+    [ApiController]
+    [Route("api/enrollments")]
+    public class EnrollmentsController : ControllerBase {
+        // GET: /<controller>/
+
+        [HttpPost]
+        public IActionResult SignUpStudentForStudies(Enrollment enrollment) {
+            if(enrollment.IndexNumber != null && enrollment.FirstName != null && enrollment.LastName != null && enrollment.BirthDate != null && enrollment.Studies != null) {
+                return Ok(enrollment);
+            } else {
+                return Ok(400);
+            }
+        }
+    }
+}
---
+++ APBD03/APBD03/Models/Enrollment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace APBD03.Models {
+
+    public class Enrollment {
+        public string IndexNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+        public string Studies { get; set; }
+
+        public Enrollment() {
+        }
+    }
+}
